339a5c65f4bef7d77b376a8a5423ac4e46ef7926
Vitor Miyazaki <vitormiya@hotmail.com>
2025-07-22 10:26:43 -0300

    feat(input): Inicia componente input

--- packages/docs/src/stories/Input.stories.ts
+++ packages/docs/src/stories/Input.stories.ts
@@ -4,10 +4,36 @@ import {Input as InputComponent, type InputProps} from "@miragem/react";
 
 
 export default {
-  title: 'Form',
-  component: InputComponent
+  title: 'Form/Input',
+  component: InputComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'line'],
+      description: 'Define o estilo visual do input'
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Desabilita o input para interação.',
+    },
+    fullWidth: {
+      control: 'boolean',
+      description: 'Faz o input ocupar 100% da largura do contêiner.',
+    },
+  }
 } as Meta<InputProps>
 
 
 
-export const Input: StoryObj<InputProps> = {}
+export const InputDefault: StoryObj<InputProps> = {
+  args: {
+    variant: 'default',
+  }
+}
+
+export const InputLine: StoryObj<InputProps> = {
+  args: {
+    variant: 'line',
+  }
+}
--- packages/react/src/components/Input.tsx
+++ packages/react/src/components/Input.tsx
@@ -1,13 +1,82 @@
-import { ComponentProps } from "react";
+import React from "react";
 import { styled } from "../styles";
+import type * as Stitches from '@stitches/react';
 
+export const InputStyled = styled('input', {
+  all: 'unset',
+  boxSizing: 'border-box',
+  width: '100%',
+  fontSize: '$md',
+  fontFamily: '$default',
+  padding: '$4',
+  color: '$gray800',
+  backgroundColor: '$white',
+  textAlign: 'center',
+  transition: 'border-color 0.2s, box-shadow 0.2s',
 
+  '&:focus': {
+    borderColor: '$blue500',
+    boxShadow: '0 0 0 2px $colors$blue300',
+  },
 
-export const Input = styled('input', {
+  '&::placeholder': {
+    color: '$gray100',
+  },
 
+  variants: {
+    variant: {
+      default: {
+        border: '1px solid $blue600',
+        color: '$gray900',
+        borderRadius: '$md',
+      },
+      line: {
+        border: 'none',
+        borderBottom: '1px solid $green500',
+        backgroundColor: '$white',
+        color: '$black',
+        textAlign: 'left',
+        borderRadius: '0',
+        padding: '0 0 $2 0'
+      },
+    },
+
+    fullWidth: {
+      true: {
+        width: '100%',
+      },
+    },
+
+    disabled: {
+      true: {
+        backgroundColor: '$gray100',
+        color: '$gray600',
+        borderColor: '$gray200',
+        cursor: 'not-allowed',
+
+        '&::placeholder': {
+          color: '$gray400',
+        }
+      },
+    }
+  },
+
+  defaultVariants: {
+    variant: 'default',
+  }
 })
 
+type InputVariants = Stitches.VariantProps<typeof InputStyled>;
+type InputVariant = 'default' | 'line';
+type NativeInputProps = React.ComponentProps<'input'>;
 
+export interface InputProps extends InputVariants, Omit<NativeInputProps, 'disabled'> {
+  variant?: InputVariant;
+  disabled?: boolean;
+}
 
-
-export type InputProps = ComponentProps<typeof Input> 
+export function Input({ disabled, ...props }: InputProps) {
+  return (
+      <InputStyled disabled={disabled} {...props} />
+  )
+}
