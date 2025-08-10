721c56f1ede045b4147075df003c825ad3f39859
Douglas Nadoti <dodo_nadoti@hotmail.com>
2025-08-10 14:50:24 -0300

    exportar component inputCurrency

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -3,6 +3,7 @@ import '../style.css';
 export { Button} from "./Button/Button"
 export { Card} from "./Card/Card"
 export { Input} from "./Input/Input"
+export { CurrencyInput } from "./InputCurrency/InputCurrency"
 export { Select} from "./Select/Select"
 export { Checkbox} from "./Checkbox/Checkbox"
 
@@ -11,3 +12,4 @@ export type { ICardProps } from "../interface/card.interface"
 export type { InputProps } from "../interface/input.interface"
 export type { SelectIProps } from "../interface/select.interface"
 export type { ICheckboxProps } from "../interface/checkbox.interface"
+export type { CurrencyInputProps } from "../interface/input-currency.interface"
